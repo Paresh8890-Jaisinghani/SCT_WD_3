82a4c810165a98dcd4f7894f46788064559e398b
Paresh8890-Jaisinghani <136781047+Paresh8890-Jaisinghani@users.noreply.github.com>
2024-08-17 19:54:50 +0530

    Files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Board from './components/board';
+import Winner from './components/winner';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Board/>
+      <Winner/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/winner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Winner =()=>{
+    return (
+        <h1>Winner</h1>
+    )
+}
+
+
+export default Winner;
